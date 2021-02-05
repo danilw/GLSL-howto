dd005e5d3f3eda37b79f872079271f20d4fbe896
Danil <s48gs.w@gmail.com>
2021-02-05 07:55:49 +0300

    Update main_image.glsl

--- test_shaders/TEST_FBO_testing/main_image.glsl
+++ test_shaders/TEST_FBO_testing/main_image.glsl
@@ -1,5 +1,3 @@
-// self https://www.shadertoy.com/view/WlcBWr
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord/iResolution.xy;
@@ -8,7 +6,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec4 col = vec4(0.);
     switch(idx){
         case 0:col=vec4(texture(iChannel0,uv).rgb,1.);break;
-        case 1:col=vec4(vec3(1.),0.);break;
+        case 1:
+          col=vec4(vec3(1.),0.);
+          float d=print_n(uv+vec2(-0.5,-0.5-0.3),iMouse.x).x;
+          d+=print_n(uv+vec2(-0.5,-0.5-0.1),iMouse.y).x;
+          d+=print_n(uv+vec2(-0.5,-0.5+0.1),iMouse.z).x;
+          d+=print_n(uv+vec2(-0.5,-0.5+0.3),iMouse.w).x;
+          col.rgb*=1.-d;col.a=d;
+          break;
         case 2:col=vec4(texture(iChannel2,uv).rgb,1.);break;
         case 3:col=vec4(texture(iChannel3,uv).rgb,1.);break;
     }
